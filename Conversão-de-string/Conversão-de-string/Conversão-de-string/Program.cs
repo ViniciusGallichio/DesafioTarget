8e6f05188a136b931d6ac6549c276090207d4760
Vinicius M Gallichio <viniciusgallichio@gmail.com>
2021-12-14 16:07:56 -0300

    First commit

---
+++ Conversão-de-string/Conversão-de-string/Conversão-de-string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Conversão_de_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            char[] conversão = a.ToCharArray();
+            string b = String.Empty;
+            for (var i = conversão.Length - 1; i >= 0; i--)
+            {
+                b += a[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Fibonacci/SequenciaFibonacci/SequenciaFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SequenciaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para saber se ele faz parte da sequência de Fibonacci.");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                a = b;
+                b = soma;
+                soma = a + b;
+
+                if (soma == n)
+                {
+                    Console.WriteLine($"Número {n} pertence a sequência.");
+                    break;
+                }
+            }
+
+            if (soma > n)
+            {
+                Console.WriteLine($"Número {n} não pertence a sequência.");
+            }
+
+        }
+    }
+}
